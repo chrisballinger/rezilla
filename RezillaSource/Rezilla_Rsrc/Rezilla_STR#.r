dce2b5db4dd30e6dfda880a676f1f11ac45bf386
bdesgraupes <bdesgraupes@a0aff4df-974a-46cd-ba52-22e6818bbca1>
2004-02-29 08:50:55 +0000

    STR# resources

---
+++ RezillaSource/Rezilla_Rsrc/Rezilla_STR#.r
@@ -0,0 +1,83 @@
+// ===========================================================================
+// Rezilla_STR#.r					
+//                       Created: 2004-02-28 22:51:23
+//             Last modification: 2004-02-28 22:55:54
+// Author: Bernard Desgraupes
+// e-mail: <bdesgraupes@easyconnect.fr>
+// www: <http://webperso.easyconnect.fr/bdesgraupes/>
+// © Copyright: Bernard Desgraupes 2003-2004
+// All rights reserved.
+// $Date$
+// $Revision$
+// ===========================================================================
+
+#include "MacTypes.r"
+
+
+resource 'STR#' (1500, "New doc titles", purgeable) {
+	{	/* array StringArray */
+		/* [1] */
+		"untitled rez map [1]",
+		/* [2] */
+		"untitled rez map",
+		/* [3] */
+		"untitled hex [1]",
+		/* [4] */
+		"untitled hex"
+	}
+};
+
+resource 'STR#' (1501, "Windows menu items") {
+	{	/* array StringArray */
+		/* [1] */
+		"Inspector Window",
+		/* [2] */
+		"Type Inspector",
+		/* [3] */
+		"Rez Inspector"
+	}
+};
+
+resource 'STR#' (1502, "Nav Services Strings") {
+	{	/* array StringArray */
+		/* [1] */
+		"Rezilla",
+		/* [2] */
+		"Save resource map as…",
+		/* [3] */
+		"Save resource as…",
+		/* [4] */
+		"untitled.rsrc",
+		/* [5] */
+		"untitled.icns",
+		/* [6] */
+		"Create new resource map…",
+		/* [7] */
+		"untitled",
+		/* [8] */
+		"Export resource map as…",
+		/* [9] */
+		"untitled.xml",
+		/* [10] */
+		"untitled.txt"
+	}
+};
+
+resource 'STR#' (1503, "File extensions") {
+	{	/* array StringArray */
+		/* [1] */
+		"",
+		/* [2] */
+		".rsrc",
+		/* [3] */
+		".xml",
+		/* [4] */
+		".txt",
+		/* [5] */
+		".r",
+		/* [6] */
+		".icns"
+	}
+};
+
+
