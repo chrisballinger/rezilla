3ee7af7580242bb4dfb10f91250b71d86492f603
bdesgraupes <bdesgraupes@a0aff4df-974a-46cd-ba52-22e6818bbca1>
2004-04-16 19:17:45 +0000

    Separated strings

--- RezillaSource/Rezilla_Rsrc/Rezilla_TEXT.r
+++ RezillaSource/Rezilla_Rsrc/Rezilla_TEXT.r
@@ -5,7 +5,10 @@ read 'TEXT' (130 , "Licence Terms", purgeable)  "Rezilla_Licence.text";
 
 // read 'TEXT' (250 , "About Text", purgeable) "Rezilla_About.text";
 data 'TEXT' (250, "About Text", purgeable) {
-$$Format("Rezilla\nResource editor \nÂ© 2003-%d by Bernard Desgraupes\n<bdesgraupes@users.sourceforge.net>\n\nRezilla project:\n<http://sourceforge.net/projects/rezilla>\n\nRezilla is a free software",  $$YEAR)
+$$Format("Rezilla\nResource editor \n\n© 2003-%d by Bernard Desgraupes\n<bdesgraupes@users.sourceforge.net>\n\nRezilla is a free software", $$YEAR)
 };
 
+data 'TEXT' (251, "Project URL", purgeable) {
+"Rezilla project:\n<http://sourceforge.net/projects/rezilla>"
+};
 
