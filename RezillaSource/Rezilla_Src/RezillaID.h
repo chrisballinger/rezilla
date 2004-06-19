98529c6553c60bdccb476bd5f0eccf9d4566a757
bdesgraupes <bdesgraupes@a0aff4df-974a-46cd-ba52-22e6818bbca1>
2004-06-19 14:49:25 +0000

    Bumped to "1.0.4a3"

--- RezillaSource/Rezilla_Src/RezillaID.h
+++ RezillaSource/Rezilla_Src/RezillaID.h
@@ -1,7 +1,7 @@
 // ===========================================================================
 // RezillaID.h					
 //                       Created: 2004-02-23 15:26:13
-//             Last modification: 2004-06-16 13:57:51
+//             Last modification: 2004-06-18 08:59:55
 // Author: Bernard Desgraupes
 // e-mail: <bdesgraupes@easyconnect.fr>
 // www: <http://webperso.easyconnect.fr/bdesgraupes/>
@@ -16,7 +16,7 @@
 #define REZILLA_MINOR		0
 #define REZILLA_SUBMINOR	4
 #define REZILLA_STAGE 		alphaStage
-#define REZILLA_DEVEL		2
+#define REZILLA_DEVEL		3
 
 // developStage	0x20
 // alphaStage	0x40
@@ -24,4 +24,4 @@
 // finalStage	0x80
 
 // Version number string
-#define REZILLA_PLC_VERSION "1.0.4a2"
+#define REZILLA_PLC_VERSION "1.0.4a3"
