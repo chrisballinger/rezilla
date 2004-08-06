714621548d1d7a706567485e5c8ec15be0b60738
bdesgraupes <bdesgraupes@a0aff4df-974a-46cd-ba52-22e6818bbca1>
2004-08-06 10:17:36 +0000

    Reformatted comments

--- RezillaSource/Rezilla_Rsrc/Rezilla_STR#.r
+++ RezillaSource/Rezilla_Rsrc/Rezilla_STR#.r
@@ -1,7 +1,7 @@
 // ===========================================================================
 // Rezilla_STR#.r					
 //                       Created: 2004-02-28 22:51:23
-//             Last modification: 2004-06-21 15:38:05
+//             Last modification: 2004-07-01 14:42:32
 // Author: Bernard Desgraupes
 // e-mail: <bdesgraupes@easyconnect.fr>
 // www: <http://webperso.easyconnect.fr/bdesgraupes/>
@@ -15,124 +15,82 @@
 
 
 resource 'STR#' (1500, "New doc titles", purgeable) {
-	{	/* array StringArray */
-		/* [1] */
-		"untitled rez map [1]",
-		/* [2] */
-		"untitled rez map",
-		/* [3] */
-		"untitled hex [1]",
-		/* [4] */
-		"untitled hex",
-		/* [5] */
-		"Comparison window [1]",
-		/* [6] */
-		"Comparison window",
-		/* [7] */
-		"untitled template editor [1]",
-		/* [8] */
-		"untitled template editor"
+	{	
+/* [1] */	"untitled rez map [1]",
+/* [2] */	"untitled rez map",
+/* [3] */	"untitled hex [1]",
+/* [4] */	"untitled hex",
+/* [5] */	"Comparison window [1]",
+/* [6] */	"Comparison window",
+/* [7] */	"untitled template editor [1]",
+/* [8] */	"untitled template editor",
+/* [9] */	"untitled TEXT editor [1]",
+/* [10] */	"untitled TEXT editor",
+/* [11] */	"untitled Aete editor [1]",
+/* [12] */	"untitled Aete editor"
 	}
 };
 
 resource 'STR#' (1501, "Windows menu items") {
-	{	/* array StringArray */
-		/* [1] */
-		"Inspector Window",
-		/* [2] */
-		"Type Inspector",
-		/* [3] */
-		"Rez Inspector"
+	{	
+/* [1] */	"Inspector Window",
+/* [2] */	"Type Inspector",
+/* [3] */	"Rez Inspector"
 	}
 };
 
 resource 'STR#' (1502, "Nav Services Strings") {
-	{	/* array StringArray */
-		/* [1] */
-		"Rezilla",
-		/* [2] */
-		"Save resource map as…",
-		/* [3] */
-		"Save resource as…",
-		/* [4] */
-		"untitled.rsrc",
-		/* [5] */
-		"untitled.icns",
-		/* [6] */
-		"Create new resource map…",
-		/* [7] */
-		"untitled",
-		/* [8] */
-		"Export resource map as…",
-		/* [9] */
-		"untitled.xml",
-		/* [10] */
-		"untitled.txt"
+	{	
+/* [1] */	"Rezilla",
+/* [2] */	"Save resource map as…",
+/* [3] */	"Save resource as…",
+/* [4] */	"untitled.rsrc",
+/* [5] */	"untitled.icns",
+/* [6] */	"Create new resource map…",
+/* [7] */	"untitled",
+/* [8] */	"Export resource map as…",
+/* [9] */	"untitled.xml",
+/* [10] */	"untitled.txt"
 	}
 };
 
 resource 'STR#' (1503, "File extensions") {
-	{	/* array StringArray */
-		/* [1] */
-		"",
-		/* [2] */
-		".rsrc",
-		/* [3] */
-		".xml",
-		/* [4] */
-		".txt",
-		/* [5] */
-		".html",
-		/* [6] */
-		".r",
-		/* [7] */
-		".icns"
+	{	
+/* [1] */	"",
+/* [2] */	".rsrc",
+/* [3] */	".xml",
+/* [4] */	".txt",
+/* [5] */	".html",
+/* [6] */	".r",
+/* [7] */	".icns"
 	}
 };
 
 
 resource 'STR#' (1504, "Project URLs") {
-	{	/* array StringArray */
-		/* [1] */
-		"Rezilla Project",
-		/* [2] */
-		"http://sourceforge.net/projects/rezilla",
-		/* [3] */
-		"Binary releases",
-		/* [4] */
-		"http://sourceforge.net/project/showfiles.php?group_id=83267",
-		/* [5] */
-		"Browse CVS repository",
-		/* [6] */
-		"http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/rezilla",
-		/* [7] */
-		"Bug Tracker",
-		/* [8] */
-		"http://sourceforge.net/tracker/?atid=568882&group_id=83267&func=browse",
-		/* [9] */
-		"Mailing list",
-		/* [10] */
-		"http://lists.sourceforge.net/lists/listinfo/rezilla-users",
-		/* [11] */
-		"by Bernard Desgraupes",
-		/* [12] */
-		"mailto:bdesgraupes@easyconnect.fr"
+	{	
+/* [1] */	"Rezilla project",
+/* [2] */	"http://sourceforge.net/projects/rezilla",
+/* [3] */	"Binary releases",
+/* [4] */	"http://sourceforge.net/project/showfiles.php?group_id=83267",
+/* [5] */	"Browse CVS repository",
+/* [6] */	"http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/rezilla",
+/* [7] */	"Bug tracker",
+/* [8] */	"http://sourceforge.net/tracker/?atid=568882&group_id=83267&func=browse",
+/* [9] */	"Subscribe to mailing list",
+/* [10] */	"http://lists.sourceforge.net/lists/listinfo/rezilla-users",
+/* [11] */	"Bernard Desgraupes",
+/* [12] */	"mailto:bdesgraupes@easyconnect.fr"
 	}
 };
 
 
 resource 'STR#' (1100, "Help Tags") {
-	{	/* array StringArray */
-		/* [1] */
-		"Copy into old map",
-		/* [2] */
-		"Copy into new map",
-		/* [3] */
-		"Copy this resource from the 'new' map in"
-		"to the 'old' one",
-		/* [4] */
-		"Copy this resource from the 'old' map in"
-		"to the 'new' one"
+	{	
+/* [1] */	"Copy into old map",
+/* [2] */	"Copy into new map",
+/* [3] */	"Copy this resource from the 'new' map into the 'old' one",	
+/* [4] */	"Copy this resource from the 'old' map into the 'new' one"
 	}
 };
 
