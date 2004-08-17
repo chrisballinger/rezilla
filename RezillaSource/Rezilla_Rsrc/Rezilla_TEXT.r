7c341634b62ff84b0d94ee7c50c5b3ab2e1954f0
bdesgraupes <bdesgraupes@a0aff4df-974a-46cd-ba52-22e6818bbca1>
2004-08-17 19:31:37 +0000

    Subtitle

--- RezillaSource/Rezilla_Rsrc/Rezilla_TEXT.r
+++ RezillaSource/Rezilla_Rsrc/Rezilla_TEXT.r
@@ -3,7 +3,7 @@
 read 'TEXT' (130 , "Licence Terms", purgeable)  "Rezilla_Licence.text";
 
 data 'TEXT' (250, "About Text", purgeable) {
-$$Format("A resource maps editor\n\n© 2003-%d", $$YEAR)
+$$Format("Resource map editor\n\n© 2003-%d", $$YEAR)
 };
 
 data 'TEXT' (251, "Free software", purgeable) {
