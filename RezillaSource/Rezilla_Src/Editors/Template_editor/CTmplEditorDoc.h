91dcd953c5359a261bf5c8034dc3fbace94a38b2
bdesgraupes <bdesgraupes@a0aff4df-974a-46cd-ba52-22e6818bbca1>
2004-11-28 15:27:32 +0000

    Removed ListenToMessage() prototype

--- RezillaSource/Rezilla_Src/Editors/Template_editor/CTmplEditorDoc.h
+++ RezillaSource/Rezilla_Src/Editors/Template_editor/CTmplEditorDoc.h
@@ -47,10 +47,6 @@ public:
 							Boolean &outEnabled, Boolean &outUsesMark,
 							UInt16 &outMark, Str255 outName );
 
-// 	virtual void		ListenToMessage(
-// 							MessageT		inMessage,
-// 							void*			ioParam);
-
 	virtual Boolean		IsModified();
 	
 	virtual StringPtr	GetDescriptor( Str255 outDescriptor ) const;
