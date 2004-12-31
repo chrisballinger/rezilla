abbef66ea008dcc68eeed189b99a22b98c2e5c91
bdesgraupes <bdesgraupes@a0aff4df-974a-46cd-ba52-22e6818bbca1>
2004-12-31 09:42:05 +0000

    Only draw triangle.

--- RezillaSource/Rezilla_Src/Editors/Edit_ICON/Controls/UPopupDrawing.h
+++ RezillaSource/Rezilla_Src/Editors/Edit_ICON/Controls/UPopupDrawing.h
@@ -1,7 +1,7 @@
 // ===========================================================================
 // UPopupDrawing.h
 //                       Created: 2004-12-11 18:57:39
-//             Last modification: 2004-12-14 14:59:55
+//             Last modification: 2004-12-31 08:27:58
 // Author: Bernard Desgraupes
 // e-mail: <bdesgraupes@easyconnect.fr>
 // www: <http://webperso.easyconnect.fr/bdesgraupes/>
@@ -15,29 +15,26 @@
 #define _H_UPopupDrawing
 #pragma once
 
-class UPopupDrawing {
-	public:
-		// These are the only calls you'll usually need to make
-		static void			DrawPopup( const Rect &inBounds, Boolean inHilited, Boolean inEnabled );
-		static void			CalculateSwatchArea( const Rect &inBounds, Rect *outSwatchR );
-		
-		// These are the specific drawing routines
-		static void			DrawPopupNormalBW( const Rect &inBounds );
-		static void			DrawPopupNormalColor( const Rect &inBounds );
-	
-		static void			DrawPopupHilitedBW( const Rect &inBounds );
-		static void			DrawPopupHilitedColor( const Rect &inBounds );
-		
-		static void			DrawPopupDimmedBW( const Rect &inBounds );
-		static void			DrawPopupDimmedColor( const Rect &inBounds );
-		
-	protected:
-		static void			DrawPopupArrow( const Rect &inBounds, SInt16 inDepth, 
-											Boolean inHilited, Boolean inEnabled );
 
-		static void			GetArrowColor( SInt16 inDepth, 
-											Boolean inHilited, Boolean inEnabled,
-											RGBColor *outColor );
+class UPopupDrawing {
+public:
+		static void		DrawPopupArrow( const Rect &inBounds, 
+									   Boolean inHilited, 
+									   Boolean inEnabled );
+		
+		static void		CalculateSwatchArea( const Rect &inBounds, 
+											Rect *outSwatchR );
+				
+protected:
+		static void		DoDrawArrow( const Rect &inBounds, 
+									SInt16 inDepth, 
+									Boolean inHilited, 
+									Boolean inEnabled );
+
+		static void		GetArrowColor( SInt16 inDepth, 
+									  Boolean inHilited, 
+									  Boolean inEnabled,
+									  RGBColor *outColor );
 
 };
 
