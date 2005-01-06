06deaf53da97f115eb4b590376b0796e87feefa9
bdesgraupes <bdesgraupes@a0aff4df-974a-46cd-ba52-22e6818bbca1>
2005-01-06 10:08:15 +0000

    REZILLA_DEBUG_BITMAP

--- RezillaSource/Rezilla_Src/Prefix/RezillaMachODebugPrefix.h
+++ RezillaSource/Rezilla_Src/Prefix/RezillaMachODebugPrefix.h
@@ -1,11 +1,11 @@
 // ===========================================================================
 // RezillaMachODebugPrefix.h				
 //                       Created: 2003-05-08 09:21:29
-//             Last modification: 2005-01-02 14:34:35
+//             Last modification: 2005-01-05 18:46:38
 // Author: Bernard Desgraupes
 // e-mail: <bdesgraupes@easyconnect.fr>
 // www: <http://webperso.easyconnect.fr/bdesgraupes/>
-// © Copyright: Bernard Desgraupes 2003, 2005
+// © Copyright: Bernard Desgraupes 2003-2005
 // All rights reserved.
 // $Date$
 // $Revision$
@@ -16,11 +16,21 @@
 #undef __STDC__
 #define __STDC__ 1
 
+// Characterize our Mach-O target
 #define REZILLA_MACH_O 1
+
+// Define to 1 if debugging the bitmap editor. This will disable the 
+// PP_Debug_VerifyFocusCache macro constant because the bitmap editor 
+// windows have their own definition for EstablishPort() which interferes 
+// with PP debugging code.
+#define REZILLA_DEBUG_BITMAP 1
 
 // Debugging code: define PP_Debug_VerifyFocusCache as 1
 // to enable focus verification. This helps diagnose
 // problems where the focus is not being invalidated
 // after changing the port state.
-
+#if REZILLA_DEBUG_BITMAP
+#define PP_Debug_VerifyFocusCache 0
+#else
 #define PP_Debug_VerifyFocusCache 1
+#endif
--- RezillaSource/Rezilla_Src/Prefix/RezillaMachOFinalPrefix.h
+++ RezillaSource/Rezilla_Src/Prefix/RezillaMachOFinalPrefix.h
@@ -1,11 +1,11 @@
 // ===========================================================================
 // RezillaMachOFinalPrefix.h				
 //                       Created: 2003-05-08 09:21:29
-//             Last modification: 2003-05-08 09:21:29
+//             Last modification: 2005-01-05 18:47:19
 // Author: Bernard Desgraupes
 // e-mail: <bdesgraupes@easyconnect.fr>
 // www: <http://webperso.easyconnect.fr/bdesgraupes/>
-// © Copyright: Bernard Desgraupes 2003
+// © Copyright: Bernard Desgraupes 2003, 2005
 // All rights reserved.
 // $Date$
 // $Revision$
@@ -16,4 +16,5 @@
 #undef __STDC__
 #define __STDC__ 1
 
+// Characterize our Mach-O target
 #define REZILLA_MACH_O 1
