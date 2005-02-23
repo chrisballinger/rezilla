a3c0a29ee1883a1d885f81125e0a221fe910c04c
bdesgraupes <bdesgraupes@a0aff4df-974a-46cd-ba52-22e6818bbca1>
2005-02-23 15:58:36 +0000

    Removed unused tool codes

--- RezillaSource/Rezilla_Rsrc/Rezilla_RidL.r
+++ RezillaSource/Rezilla_Rsrc/Rezilla_RidL.r
@@ -1,11 +1,11 @@
 // ===========================================================================
 // Rezilla_RidL.r
 //                       Created: 2004-12-28 08:17:46
-//             Last modification: 2004-12-28 15:03:26
+//             Last modification: 2005-02-23 09:05:37
 // Author: Bernard Desgraupes
 // e-mail: <bdesgraupes@easyconnect.fr>
 // www: <http://webperso.easyconnect.fr/bdesgraupes/>
-// © Copyright: Bernard Desgraupes 2004
+// © Copyright: Bernard Desgraupes 2004, 2005
 // All rights reserved.
 // $Date$
 // $Revision$
@@ -20,31 +20,27 @@ type 'RidL' {
 };
 
 
+// RidL_ToolList
 resource 'RidL' (300, "Icon editor broadcasters (tools)") {
-	{	/* array ResourceIDList: 22 elements */
-/* [1]  ok   */		1,
-/* [2]  cncl */		2,
-/* [3]  rev  */		3,
-/* [4]  ENCL */		1162756940,
-/* [5]  tLAS */		1951154515,
-/* [6]  tSEL */		1951614284,
-/* [7]  tTXT */		1951684692,
-/* [8]  tPEN */		1951417678,
-/* [9]  tERA */		1950700097,
-/* [10] tBUC */		1950504259,
-/* [11] tDRO */		1950634575,
-/* [12] tLIN */		1951156558,
-/* [13] tREC */		1951548739,
-/* [14] tFRC */		1950765635,
-/* [15] tRRC */		1951552067,
-/* [16] tFRR */		1950765650,
-/* [17] tOVL */		1951356492,
-/* [18] tFOV */		1950764886,
-/* [19] COOR */		1129271122,
-/* [20] tPAT */		1951416660,
-/* [21] tBCL */		1950499660,
-/* [22] tFCL */		1950761804,
-/* [23] tHOT */		1950895956
+	{	/* array ResourceIDList: 18 elements */
+/* [1]  tLAS */		1951154515,
+/* [2]  tSEL */		1951614284,
+/* [3]  tTXT */		1951684692,
+/* [4]  tPEN */		1951417678,
+/* [5]  tERA */		1950700097,
+/* [6]  tBUC */		1950504259,
+/* [7]  tDRO */		1950634575,
+/* [8]  tLIN */		1951156558,
+/* [9]  tREC */		1951548739,
+/* [10] tFRC */		1950765635,
+/* [11] tRRC */		1951552067,
+/* [12] tFRR */		1950765650,
+/* [13] tOVL */		1951356492,
+/* [14] tFOV */		1950764886,
+/* [15] tHOT */		1950895956,
+/* [16] tPAT */		1951416660,
+/* [17] tBCL */		1950499660,
+/* [18] tFCL */		1950761804
 	}
 };
 
@@ -58,17 +54,17 @@ resource 'RidL' (301, "Icon editor broadcasters (cicn)") {
 };
 
 
-resource 'RidL' (302, "Icon editor broadcasters (fam)") {
+resource 'RidL' (302, "Icon editor broadcasters (icon family)") {
 	{	/*  array ResourceIDList: 12 elements */
-/* [1] icl8 */	1768123448,
-/* [2] icl4 */	1768123444,
-/* [3] ICN# */	1229147683,
-/* [4] MSK1 */	1297304369,
-/* [5] ics8 */	1768125240,
-/* [6] ics4 */	1768125236,
-/* [7] ics# */	1768125219,
-/* [8] MSK2 */	1297304370,
-/* [9] icm8 */	1768123704,
+/* [1]  icl8 */	1768123448,
+/* [2]  icl4 */	1768123444,
+/* [3]  ICN# */	1229147683,
+/* [4]  MSK1 */	1297304369,
+/* [5]  ics8 */	1768125240,
+/* [6]  ics4 */	1768125236,
+/* [7]  ics# */	1768125219,
+/* [8]  MSK2 */	1297304370,
+/* [9]  icm8 */	1768123704,
 /* [10] icm4 */	1768123700,
 /* [11] icm# */	1768123683,
 /* [12] MSK3 */	1297304371
