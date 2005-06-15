16a80580a02982696281d26efbd2ed0f4da12ad1
bdesgraupes <bdesgraupes@a0aff4df-974a-46cd-ba52-22e6818bbca1>
2005-06-15 05:20:07 +0000

    Differing IDs

--- RezillaExtras/Tests/AppleScript_Tests/RezillaScripting_Tests/Forks/BuildRezillaTestForks.tcl
+++ RezillaExtras/Tests/AppleScript_Tests/RezillaScripting_Tests/Forks/BuildRezillaTestForks.tcl
@@ -36,17 +36,17 @@ foreach tmp::suffix [list DF RF] {
 	# TEXT
 	resource write -id 128 -name "Hello" -file $tmp::rid TEXT "Hello Rezilla ($tmp::whichfork)!"
 	resource write -id 129 -name "Vivat" -file $tmp::rid TEXT "Viva Rezilla!"
-	resource write -id 130 -name "$tmp::suffix wishes" -file $tmp::rid TEXT "Happy rezing with Rezilla!"
+	resource write -id [expr {($tmp::suffix eq "RF")? 130:131}] -name "$tmp::suffix wishes" -file $tmp::rid TEXT "Happy rezing with Rezilla!"
 	# STR#
-	resource write -id 129 -name "Greetings" -file $tmp::rid "STR#" "\x00\x02\x05Hello\x05Salud"
+	resource write -id 129 -name "$tmp::suffix Greetings" -file $tmp::rid "STR#" "\x00\x02\x05Hello\x05Salud"
 	# MENU
 	set hexdata  "\x00\x80\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff\xff\xff\x01\x14\x0e\x41\x62\x6f\x75\x74\x20\x52\x65\x7a\x69\x6c\x6c\x61\xc9\x00\x00\x00\x00\x00"
 	resource write -id 128 -name "Apple menu" -file $tmp::rid MENU $hexdata
 	set hexdata  "\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff\xff\xff\x05\x53\x74\x79\x6c\x65\x05\x50\x6c\x61\x69\x6e\x00\x00\x00\x00\x04\x42\x6f\x6c\x64\x00\x00\x00\x01\x06\x49\x74\x61\x6c\x69\x63\x00\x00\x00\x02\x09\x55\x6e\x64\x65\x72\x6c\x69\x6e\x65\x00\x00\x00\x04\x00"
 	resource write -id 129 -name "Style menu" -file $tmp::rid MENU $hexdata
-	# ALRT - this resource intentionally has no name
+	# ALRT
 	set hexdata "\x00\x28\x00\x28\x00\x87\x01\x7c\x00\x80\x55\x55\x30\x0a"
-	resource write -id 128 -file $tmp::rid ALRT $hexdata
+	resource write -id 128 -name "Alert" -file $tmp::rid ALRT $hexdata
 	# MPSR - this resource intentionally has no name
 	set hexdata "\x00\x09\x4d\x6f\x6e\x61\x63\x6f\x00\x4c\x25\xb0\x00\x05\x00\x00\xbf\x80\x00\x00\x00\x00\x00\x00\x43\xcc\x80\x00\x00\x00\x00\x00\x00\x00\x00\x06\x00\x04\x00\x3b\x00\x1e\x01\xbe\x02\xff\x00\x3b\x00\x1e\x01\xbe\x02\xff\xbd\x5f\xe3\x20\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00"
 	resource write -id 1005 -file $tmp::rid MPSR $hexdata
