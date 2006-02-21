8c04cc18badfaba8ee3fb18e40a5f41c3dc1c517
bdesgraupes <bdesgraupes@a0aff4df-974a-46cd-ba52-22e6818bbca1>
2006-02-21 06:59:48 +0000

    First checkin

---
+++ RezillaSource/Rezilla_SDK/SamplePlugin/RezSamplePluginID.h
@@ -0,0 +1,15 @@
+// ===========================================================================
+// File: "RezSamplePluginID.h"
+// 
+//                        Created: 2006-02-17 14:35:33
+//              Last modification: 2006-02-19 07:37:05
+// Author: Bernard Desgraupes
+// e-mail: <bdesgraupes@users.sourceforge.net>
+// www: <http://rezilla.sourceforge.net/>
+// (c) Copyright: Bernard Desgraupes 2006
+// All rights reserved.
+// ===========================================================================
+
+// Version number for this plugin
+#define SAMPLE_PLUGIN_VERSION "0.2"
+
