e71622f1d55ab1cbd692d3ffdaffdfef9759607d
bdesgraupes <bdesgraupes@a0aff4df-974a-46cd-ba52-22e6818bbca1>
2006-02-22 14:49:13 +0000

    First checkin

---
+++ RezillaSource/Rezilla_SDK/SamplePlugin/RezSamplePlugin_MENU.r
@@ -0,0 +1,27 @@
+// ===========================================================================
+// File: "RezSamplePlugin_MENU.r"
+//                        Created: 2006-02-22 11:56:51
+//              Last modification: 2006-02-22 11:56:51
+// Author: Bernard Desgraupes
+// e-mail: <bdesgraupes@users.sourceforge.net>
+// www: <http://rezilla.sourceforge.net/>
+// (c) Copyright: Bernard Desgraupes 2006
+// All rights reserved.
+// ===========================================================================
+
+
+
+resource 'MENU' (500, "Sample plugin menu") {
+	0,
+	textMenuProc,
+	0x3,
+	enabled,
+	"Sample",
+	{	/* array: 2 elements */
+		/* [1] */
+		"Reverse string", noIcon, noKey, noMark, plain,
+		/* [2] */
+		"Rotate string", noIcon, noKey, noMark, plain
+	}
+};
+
