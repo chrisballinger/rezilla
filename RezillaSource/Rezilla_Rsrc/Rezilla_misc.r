1d495e88c1884a62295d61053b14dfa830c83e43
bdesgraupes <bdesgraupes@a0aff4df-974a-46cd-ba52-22e6818bbca1>
2006-03-11 18:54:11 +0000

    Moved RzII to Rezilla_icons.r

--- RezillaSource/Rezilla_Rsrc/Rezilla_misc.r
+++ RezillaSource/Rezilla_Rsrc/Rezilla_misc.r
@@ -1,34 +1,16 @@
 // ===========================================================================
 // Rezilla_misc.r
 //                       Created: 2004-02-28 22:51:23
-//             Last modification: 2004-12-28 15:03:33
+//             Last modification: 2006-03-10 16:39:03
 // Author: Bernard Desgraupes
 // e-mail: <bdesgraupes@users.sourceforge.net>
 // www: <http://rezilla.sourceforge.net/>
-// (c) Copyright: Bernard Desgraupes 2003-2004
+// (c) Copyright: Bernard Desgraupes 2003-2004, 2006
 // All rights reserved.
 // ===========================================================================
 
 #include <Carbon/Carbon.r>
 
-
-// Rezilla Icon family info type
-type 'RzII' {
-	literal longint;	/* defaultPane */
-	longint = $$CountOf(EntriesList);
-	wide array EntriesList {
-		literal longint;	/* rezType */
-		longint;            /* flags */
-		longint;            /* width */
-		longint;            /* height */
-		longint;            /* depth */
-		longint;            /* rowBytes */
-		literal longint;	/* samplePaneID */
-		longint;            /* maskOffset */
-		longint;            /* maskRowBytes */
-		literal longint;	/* maskSamplePaneID */
-	};
-};
 
 type 'TMPL' {
 	array {
@@ -76,55 +58,6 @@ resource 'DITL' (204, locked, preload) {
 };
 // End of PP Customization
 
-
-// These icon family information resources (cf type definition above) must
-// have the same ID as the corresponding PPob resources
-// (PPob_IconSuiteEditor and PPob_ICONEditor). They are used by the
-// CWindow_Family class constructor.
-resource 'RzII' (10600, "Family view info") {
-	'icl8',
-	{	/* array EntriesList: 9 elements */
-/* [1] */	'icl8', 0, 32, 32, 8, 32, 'icl8', 0, 0, '????',
-/* [2] */	'icl4', 0, 32, 32, 4, 16, 'icl4', 0, 0, '????',
-/* [3] */	'ICN#', 0, 32, 32, 1, 4, 'ICN#', 128, 4, 'MSK1',
-/* [4] */	'ics8', 0, 16, 16, 8, 16, 'ics8', 0, 0, '????',
-/* [5] */	'ics4', 0, 16, 16, 4, 8, 'ics4', 0, 0, '????',
-/* [6] */	'ics#', 0, 16, 16, 1, 2, 'ics#', 32, 2, 'MSK2',
-/* [7] */	'icm8', 0, 16, 12, 8, 16, 'icm8', 0, 0, '????',
-/* [8] */	'icm4', 0, 16, 12, 4, 8, 'icm4', 0, 0, '????',
-/* [9] */	'icm#', 0, 16, 12, 1, 2, 'icm#', 24, 2, 'MSK3'
-	}
-};
-
-
-resource 'RzII' (10602, "Icon view info") {
-	'ICON',
-	{	/* array EntriesList: 1 elements */
-/* [1] */		'ICON', 0, 32, 32, 1, 4, 'ICON', 0, 0, '????'
-	}
-};
-
-
-// Template for the 'RzII' resource type
-resource 'TMPL' (1000, "RzII", purgeable) {
-	{
-		"Rezilla icon family info", 'DVDR',
-		"Default pane",				'TNAM',
-		"Count:",					'LCNT',
-		"Icons",					'LSTC',
-		"Rez type",					'TNAM',
-		"Flags",					'DLNG',
-		"Width",					'DLNG',
-		"Height",					'DLNG',
-		"Depth",					'DLNG',
-		"Row bytes",				'DLNG',
-		"Sample pane ID",			'TNAM',
-		"Mask offset",				'DLNG',
-		"Mask row bytes",			'DLNG',
-		"Mask sample pane ID",		'TNAM',
-		"*****",					'LSTE'
-	}
-};
 
 
 // Used by the PatternPane in the Icon editor
