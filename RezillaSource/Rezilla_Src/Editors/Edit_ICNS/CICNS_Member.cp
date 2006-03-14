d4a0ed7308da498d59ea78102d0f19e34a05add2
bdesgraupes <bdesgraupes@a0aff4df-974a-46cd-ba52-22e6818bbca1>
2006-03-14 21:53:45 +0000

    mModified

--- RezillaSource/Rezilla_Src/Editors/Edit_ICNS/CICNS_Member.cp
+++ RezillaSource/Rezilla_Src/Editors/Edit_ICNS/CICNS_Member.cp
@@ -2,28 +2,13 @@
 // CICNS_Member.cp
 // 
 //                       Created: 2006-02-23 15:12:16
-//             Last modification: 2006-02-26 12:03:47
+//             Last modification: 2006-03-13 15:56:04
 // Author: Bernard Desgraupes
 // e-mail: <bdesgraupes@users.sourceforge.net>
 // www: <http://rezilla.sourceforge.net/>
 // (c) Copyright : Bernard Desgraupes, 2006
 // All rights reserved.
 // ===========================================================================
-// 	type 'icns' {
-// 		iconFamilyBegin:
-// 			literal longint = 'icns';
-// 			unsigned longint = (iconFamilyEnd - iconFamilyBegin) >> 3;
-// 			array elementArray {
-// 				literal longint;
-// 				elementBegin:
-// 					longint =   (elementEnd[$$ArrayIndex(elementArray)] 
-// 						- elementBegin[$$ArrayIndex(elementArray)] >> 3) + 4;
-// 					hex string;
-// 				elementEnd:
-// 			};
-// 		iconFamilyEnd:
-// 	};
-			 
 
 #include "CICNS_Member.h"
 #include "RezillaConstants.h"
@@ -31,24 +16,16 @@
 
 #include <LHandleStream.h>
 
-// ---------------------------------------------------------------------------
-//  CICNS_Member													[public]
-// ---------------------------------------------------------------------------
-
-CICNS_Member::CICNS_Member(OSType inType)
-{
-	SetValues(inType, 0, NULL);
-}
-
 
 // ---------------------------------------------------------------------------
 //  CICNS_Member													[public]
 // ---------------------------------------------------------------------------
 // inSize should be the size of inHandle
 
-CICNS_Member::CICNS_Member(OSType inType, SInt32 inSize, Handle inHandle)
+CICNS_Member::CICNS_Member(OSType inType, Handle inHandle)
 {
-	SetValues(inType, inSize, inHandle);
+	mModified = false;
+	SetValues(inType, inHandle);
 }
 
 
@@ -69,10 +46,9 @@ CICNS_Member::~CICNS_Member()
 // ---------------------------------------------------------------------------
 
 void
-CICNS_Member::GetValues(OSType & outType, SInt32 & outSize, Handle & outHandle)
+CICNS_Member::GetValues(OSType & outType, Handle & outHandle)
 {
 	outType = mType;
-	outSize = mIconSize;
 	outHandle = mIconData;
 }
  
@@ -82,10 +58,9 @@ CICNS_Member::GetValues(OSType & outType, SInt32 & outSize, Handle & outHandle)
 // ---------------------------------------------------------------------------
 
 void
-CICNS_Member::SetValues(OSType inType, SInt32 inSize, Handle inHandle)
+CICNS_Member::SetValues(OSType inType, Handle inHandle)
 {
 	mType = inType;
-	mIconSize = inSize;
 	
 	if (inHandle == nil) {
 		mIconData = NewHandle(0);
--- RezillaSource/Rezilla_Src/Editors/Edit_ICNS/CICNS_Member.h
+++ RezillaSource/Rezilla_Src/Editors/Edit_ICNS/CICNS_Member.h
@@ -2,20 +2,41 @@
 // CICNS_Member.h
 // 
 //                       Created: 2006-02-23 15:12:16
-//             Last modification: 2006-03-11 17:33:31
+//             Last modification: 2006-03-13 15:55:55
 // Author: Bernard Desgraupes
 // e-mail: <bdesgraupes@users.sourceforge.net>
 // www: <http://rezilla.sourceforge.net/>
 // (c) Copyright : Bernard Desgraupes, 2006
 // All rights reserved.
 // ===========================================================================
+// 	type 'icns' {
+// 		iconFamilyBegin:
+// 			literal longint = 'icns';
+// 			unsigned longint = (iconFamilyEnd - iconFamilyBegin) >> 3;
+// 			array elementArray {
+// 				literal longint;
+// 				elementBegin:
+// 					longint =   (elementEnd[$$ArrayIndex(elementArray)] 
+// 						- elementBegin[$$ArrayIndex(elementArray)] >> 3) + 4;
+// 					hex string;
+// 				elementEnd:
+// 			};
+// 		iconFamilyEnd:
+// 	};
+// 	
+// 	
 // Icons.h in the ApplicationServices framework defines the
 // IconFamilyElement struct like this:
+// 
 // 		struct IconFamilyElement {
 // 		  OSType              elementType;      /* 'ICN#', 'icl8', etc...*/
 // 		  Size                elementSize;      /* Size of this element*/
 // 		  unsigned char       elementData[1];
 // 		};
+// 		
+// Note: elementSize includes the size of the OSType, the size of the
+// SInt32 and the size of the compressed icon
+
 
 
 #ifndef _H_CICNS_Member
@@ -27,35 +48,25 @@ class LHandleStream;
 class CICNS_Member {
 
 public:
-					CICNS_Member(OSType inType);
-					CICNS_Member(OSType inType, SInt32 inSize, Handle inHandle);
-// 					CICNS_Member(LHandleStream * inStream);
+					CICNS_Member(OSType inType, Handle inHandle = NULL);
 					~CICNS_Member();
 
-// 	virtual void	InstallDataStream(LHandleStream * inStream);
-// 	virtual void	SendDataToStream(LHandleStream * outStream);
-
-	void			GetValues(OSType & outType, SInt32 & outSize, Handle & outHandle);
-	void			SetValues(OSType inType, SInt32 inSize, Handle inHandle);
+	void			GetValues(OSType & outType, Handle & outHandle);
+	void			SetValues(OSType inType, Handle inHandle);
 
 	OSType			GetType() { return mType;}
-
-	SInt32			GetIconSize() { return mIconSize;}
-	void			SetIconSize(SInt32 inIconSize) {mIconSize = inIconSize;}
 
 	virtual Handle	GetIconData() { return mIconData;}
 	void			SetIconData(Handle inIconData) {mIconData = inIconData;}
 
+	virtual Boolean	IsModified() { return mModified;}
+	void			SetModified(Boolean inModified) {mModified = inModified;}
+
 protected:
 		OSType			mType;		// Like 'it32', 'ih32', 'tile' etc.
-		SInt32			mIconSize;	// Size of the icon member
-		      			      		// Warning: 
-		      			      		//   this includes the size of the 
-		      			      		//   OSType, the size of this SInt32 
-		      			      		//   and the size of the compressed 
-		      			      		//   icon
 		Handle			mIconData;	// A handle to the icon member 
 		      			          	// data after decompression
+		Boolean			mModified;		// Like 'it32', 'ih32', 'tile' etc.
 
 private:
 
