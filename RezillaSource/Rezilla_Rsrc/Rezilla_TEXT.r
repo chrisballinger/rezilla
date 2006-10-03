f5e714fe2e01578fd3b1de3e814a512ad62b750b
bdesgraupes <bdesgraupes@a0aff4df-974a-46cd-ba52-22e6818bbca1>
2006-10-03 13:16:57 +0000

    License typo

--- RezillaSource/Rezilla_Rsrc/Rezilla_TEXT.r
+++ RezillaSource/Rezilla_Rsrc/Rezilla_TEXT.r
@@ -1,17 +1,17 @@
 // ===========================================================================
 // Rezilla_TEXT.r
 //                       Created: 2004-02-28 22:51:23
-//             Last modification: 2004-12-11 11:51:11
+//             Last modification: 2006-09-30 17:10:51
 // Author: Bernard Desgraupes
 // e-mail: <bdesgraupes@users.sourceforge.net>
 // www: <http://rezilla.sourceforge.net/>
-// (c) Copyright: Bernard Desgraupes 2003-2004
+// (c) Copyright: Bernard Desgraupes 2003-2006
 // All rights reserved.
 // ===========================================================================
 
 #include <MacTypes.r>
 
-read 'TEXT' (130 , "Licence Terms", purgeable)  "Rezilla_License.text";
+read 'TEXT' (130 , "License Terms", purgeable)  "Rezilla_License.text";
 
 data 'TEXT' (250, "About Text", purgeable) {
 $$Format("Resource map editor\n\n(c) 2003-%d", $$YEAR)
