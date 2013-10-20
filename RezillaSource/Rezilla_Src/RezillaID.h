b5b56d66ba8ff899e1f862902104615966b24779
bdesgraupes <bdesgraupes@a0aff4df-974a-46cd-ba52-22e6818bbca1>
2013-10-20 08:45:27 +0000

    Bumped to version 1.3b1

--- RezillaSource/Rezilla_Src/RezillaID.h
+++ RezillaSource/Rezilla_Src/RezillaID.h
@@ -1,11 +1,11 @@
 // ===========================================================================
 // RezillaID.h		
 //                       Created: 2004-02-23 15:26:13
-//             Last modification: 2006-11-28 10:28:51
+//             Last modification: 2013-10-20 09:50:17
 // Author: Bernard Desgraupes
 // e-mail: <bdesgraupes@users.sourceforge.net>
 // www: <http://rezilla.sourceforge.net/>
-// (c) Copyright: Bernard Desgraupes 2004-2006
+// (c) Copyright: Bernard Desgraupes 2004-2013
 // All rights reserved.
 // ===========================================================================
 // developStage	0x20
@@ -15,10 +15,10 @@
 
 
 #define REZILLA_MAJOR 		1
-#define REZILLA_MINOR		2
+#define REZILLA_MINOR		3
 #define REZILLA_SUBMINOR	0
 #define REZILLA_STAGE 		betaStage
-#define REZILLA_DEVEL		4
+#define REZILLA_DEVEL		1
 
 // Version number string
-#define REZILLA_PLC_VERSION "1.2b4"
+#define REZILLA_PLC_VERSION "1.3b1"
